19e0bc63231e726d27818dc78793e5132ab8a325
unknown <ewendel88@gmail.com>
2020-02-27 22:07:47 -0600

    Added some day/night and weather stuff.

--- Sand Dune/Assets/MyAssets/Scripts/DayNightCycle.cs
+++ Sand Dune/Assets/MyAssets/Scripts/DayNightCycle.cs
@@ -8,15 +8,42 @@ public class DayNightCycle : MonoBehaviour {
 	[SerializeField] float cycleLength;
 	[SerializeField] float currentSecondsOfTheDay;
 	[SerializeField] float dayTotalSeconds = 86400;
+	[SerializeField] float dayPercent;
+	[SerializeField] Light sun;
+	[SerializeField] Material skyboxDay;
+	[SerializeField] Material skyboNight;
+	[SerializeField] AnimationCurve intensity;
 
 	private float sunSeasonAngle;
 	
 	void Update () {
+		float dayNightCurveValue = Utility.EvaluateCurve(intensity, dayPercent);
 		if (currentSecondsOfTheDay >= dayTotalSeconds)
 			currentSecondsOfTheDay = currentSecondsOfTheDay - dayTotalSeconds;			
 		currentSecondsOfTheDay += Time.deltaTime * dayTotalSeconds / cycleLength;
-		transform.eulerAngles = new Vector3((currentSecondsOfTheDay/dayTotalSeconds * 360) - 90f, 0f, 0f);
+		dayPercent = currentSecondsOfTheDay/dayTotalSeconds;
+		transform.eulerAngles = new Vector3((dayPercent * 360) - 90f, 0f, 0f);
+
+		sun.intensity = dayNightCurveValue;
+
+		RenderSettings.skybox = dayNightCurveValue == 0 ? skyboNight : skyboxDay; // need to smoothly blend
+
+		// This is probably overkill. maybe just make this a animationcurve
+		// if (dayPercent <= .255)
+		// 	sun.intensity = 100 * Mathf.Abs(dayPercent - .25f) + 100 * (dayPercent - .25f);
+		// else if (dayPercent >= .745)
+		// 	sun.intensity = (-100 * Mathf.Abs(dayPercent - .745f) - 100 * (dayPercent - .745f)) + 1;
+		// else
+		// 	sun.intensity = 1;
 
 		timeOfDay = Mathf.Floor(currentSecondsOfTheDay / 3600).ToString("00") + ":" + (Mathf.Floor(currentSecondsOfTheDay / 60) - Mathf.Floor(currentSecondsOfTheDay / 3600) * 60).ToString("00");
+	}
+
+	void SunSet() {
+
+	}
+
+	void SunRise() {
+		
 	}
 }
--- Sand Dune/Assets/MyAssets/Scripts/Utility.cs
+++ Sand Dune/Assets/MyAssets/Scripts/Utility.cs
@@ -17,9 +17,9 @@ public class Utility {
 	/// <summary>Computes and returns the value of a given ratio on a given curve applied to a given maximum.</summary>
 	/// <param name ="curve">The curve on which the ratio will be computed.</param>
 	/// <param name="ratio">The value to compute on the curve. Usually a float between 0 and 1.</param>
-	/// <param name="maximum">The maximum value that should be returned. Return value will be relative to maximum.</param>
-	/// <returns>Returns the Y-value of the curve at the given X-value as a float multiplied by the constant maximum.</returns>
-	public static float EvaluateCurve(AnimationCurve curve, float ratio, float maximum) {
-		return curve.Evaluate(ratio) * maximum;
+	/// <param name="multiplier">The maximum value that should be returned. Return value will be relative to multiplier.</param>
+	/// <returns>Returns the Y-value of the curve at the given X-value as a float multiplied by the constant multiplier.</returns>
+	public static float EvaluateCurve(AnimationCurve curve, float ratio, float multiplier) {
+		return curve.Evaluate(ratio) * multiplier;
 	}
 }
